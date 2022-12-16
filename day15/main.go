fc0b27a914f80d5f6fa7baaf25f6c5b37495305b
scnace <scbizu@gmail.com>
2022-12-16 11:26:19 +0800

    day15: line merge and manhattan distance (forceburst)

---
+++ day15/main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/magejiCoder/set"
+	"github.com/scbizu/aoc2022/helper/grid"
+	"github.com/scbizu/aoc2022/helper/input"
+)
+
+var lineExpr = "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d"
+
+var ls = []*grid.HorizontalLine{}
+
+var (
+	minX, maxX = 0, 4000000
+	minY, maxY = 0, 4000000
+)
+
+var y = 2000000
+
+var (
+	count int
+	b     = set.New[grid.Vec]()
+)
+
+func main() {
+	input.NewTXTFile("input.txt").ReadByLine(context.Background(), handler)
+	ls = grid.Merge(ls)
+	var count int
+	for _, l := range ls {
+		l.Print()
+		count += l.Len()
+	}
+	println("minY:", minY)
+	fmt.Fprintf(os.Stdout, "count: %d\n", count-b.Size())
+
+	input.NewTXTFile("input.txt").ReadByLine(context.Background(), handler2)
+
+	println("p2:")
+
+	for i, l := range lines {
+		l = grid.Merge(l)
+		if len(l) > 1 {
+			for _, l2 := range l {
+				l2.Print()
+			}
+			fmt.Fprintf(os.Stdout, "x: %d\n", (l[0].ToX()+1)*4000000+i)
+			break
+		}
+	}
+}
+
+func handler(line string) error {
+	var sensor grid.Vec
+	var beacon grid.Vec
+	if _, err := fmt.Sscanf(line, lineExpr, &sensor.X, &sensor.Y, &beacon.X, &beacon.Y); err != nil {
+		return err
+	}
+	distance := grid.Distance(sensor, beacon)
+
+	if sensor.Y+distance < y || sensor.Y-distance > y {
+		return nil
+	}
+
+	var x0, x1 int
+	if sensor.Y <= y {
+		x0 = sensor.X - (distance - y + sensor.Y)
+		x1 = sensor.X + (distance - y + sensor.Y)
+	} else {
+		x0 = sensor.X - (distance + y - sensor.Y)
+		x1 = sensor.X + (distance + y - sensor.Y)
+	}
+	var temp int
+	if x0 < x1 {
+		temp = x0
+		x0 = x1
+		x1 = temp
+	}
+
+	ls = append(ls, grid.NewHorizontalLine(x1, x0, y))
+
+	if beacon.Y == y && !b.Has(beacon) {
+		b.Add(beacon)
+	}
+
+	return nil
+}
+
+var lines = make(map[int][]*grid.HorizontalLine)
+
+func handler2(line string) error {
+	var sensor grid.Vec
+	var beacon grid.Vec
+	if _, err := fmt.Sscanf(line, lineExpr, &sensor.X, &sensor.Y, &beacon.X, &beacon.Y); err != nil {
+		return err
+	}
+
+	distance := grid.Distance(sensor, beacon)
+	y1, y2 := sensor.Y-distance, sensor.Y+distance
+
+	if y1 < minY {
+		y1 = minY
+	}
+	if y2 > maxY {
+		y2 = maxY
+	}
+
+	for y := y1; y <= y2; y++ {
+		var x0, x1 int
+		if sensor.Y <= y {
+			x0 = sensor.X - (distance - y + sensor.Y)
+			x1 = sensor.X + (distance - y + sensor.Y)
+		} else {
+			x0 = sensor.X - (distance + y - sensor.Y)
+			x1 = sensor.X + (distance + y - sensor.Y)
+		}
+		var temp int
+		if x0 < x1 {
+			temp = x0
+			x0 = x1
+			x1 = temp
+		}
+
+		if x0 > maxX {
+			x0 = maxX
+		}
+		if x1 < minX {
+			x1 = minX
+		}
+		lines[y] = append(lines[y], grid.NewHorizontalLine(x1, x0, y))
+	}
+
+	return nil
+}
--- helper/grid/line.go
+++ helper/grid/line.go
@@ -1,5 +1,7 @@
 package grid
 
+import "sort"
+
 type Line struct {
 	from Vec
 	to   Vec
@@ -7,6 +9,14 @@ type Line struct {
 
 func NewLine(v1, v2 Vec) *Line {
 	return &Line{from: v1, to: v2}
+}
+
+func (l *Line) ToX() int {
+	return l.to.X
+}
+
+func (l *HorizontalLine) Print() {
+	println(l.from.X, l.to.X)
 }
 
 func (l *Line) OnDraw(fn func(v Vec) error) error {
@@ -30,4 +40,70 @@ func (l *Line) OnDraw(fn func(v Vec) error) error {
 		}
 	}
 	return nil
+}
+
+type HorizontalLine struct {
+	*Line
+}
+
+func NewHorizontalLine(x0, x1, y int) *HorizontalLine {
+	return &HorizontalLine{NewLine(Vec{x0, y}, Vec{x1, y})}
+}
+
+func (l *HorizontalLine) Add(l2 *HorizontalLine) []*HorizontalLine {
+	if l.from.Y != l2.from.Y {
+		panic("not horizontal")
+	}
+	if l2.from.X > l.to.X+1 {
+		return []*HorizontalLine{l, l2}
+	}
+	if l.from.X > l2.to.X+1 {
+		return []*HorizontalLine{l2, l}
+	}
+	if l.from.X > l2.from.X {
+		l.from.X = l2.from.X
+	}
+	if l.to.X < l2.to.X {
+		l.to.X = l2.to.X
+	}
+	return []*HorizontalLine{l}
+}
+
+func (l *HorizontalLine) Len() int {
+	return l.to.X - l.from.X + 1
+}
+
+type HorizontalLines []*HorizontalLine
+
+func (hls HorizontalLines) Len() int {
+	return len(hls)
+}
+
+func (hls HorizontalLines) Less(i, j int) bool {
+	return hls[i].from.X < hls[j].from.X
+}
+
+func (hls HorizontalLines) Swap(i, j int) {
+	hls[i], hls[j] = hls[j], hls[i]
+}
+
+func Merge(hls HorizontalLines) HorizontalLines {
+	if len(hls) == 1 {
+		return hls
+	}
+	sort.Sort(hls)
+	merged := []*HorizontalLine{hls[0]}
+	res := []*HorizontalLine{}
+	for i := 1; i < len(hls); i++ {
+		if i == len(hls)-1 {
+			res = append(res, merged...)
+		}
+		merged = merged[len(merged)-1].Add(hls[i])
+		if len(merged) == 2 {
+			res = append(res, merged[0])
+			merged = merged[1:]
+		}
+	}
+
+	return res
 }
